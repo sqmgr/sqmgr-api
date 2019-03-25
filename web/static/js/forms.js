761f7fbd72ee66e4b8b915b1d6907a4ff3fe97ff
Thomas Peters <weters@me.com>
2019-03-24 23:19:03 -0400

    Rename forms2.js to froms.js

--- web/static/js/forms.js
+++ web/static/js/forms.js
@@ -14,153 +14,51 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-Date.prototype.toLocalTimeString = function() {
-	var pad = function(x) {
-		return x < 10 ? '0' + x : x
-	}
-
-	return pad(this.getHours()) +
-		':' + pad(this.getMinutes())
-}
-
-Date.prototype.toLocalYMDString = function() {
-	var pad = function(x) {
-		return x < 10 ? '0' + x : x
-	}
-
-	return this.getFullYear() +
-		'-' + pad(this.getMonth()) +
-		'-' + pad(this.getDate())
-}
-
 window.addEventListener('load', function() {
-	var now = new Date()
-	var today = new Date(now.getFullYear(), now.getMonth(), now.getDate())
-
-	document.querySelector('input[name="timezone-offset"]').value = -new Date().getTimezoneOffset()/60
-
-	document.querySelectorAll('input').forEach(function(input) {
-		var check = function() {
-			if (this.value === "") {
-				this.classList.add('empty')
-				this.classList.remove('not-empty')
-			} else {
-				this.classList.add('not-empty')
-				this.classList.remove('empty')
-			}
-		}
-
-		input.addEventListener('blur', check)
-		input.addEventListener('keyup', check)
-		check.call(input)
-	})
-
-	document.querySelectorAll('input[type="date"]').forEach(function(input) {
-		var dataDefault = input.getAttribute('data-default') || ''
-
-		if (dataDefault === 'today') {
-			input.value = today.toLocalYMDString()
-		}
-	})
-
-	document.querySelectorAll('input[type="time"]').forEach(function(input) {
-		var dataDefault = input.getAttribute('data-default') || ''
-
-		if (dataDefault === 'today') {
-			input.value = today.toLocalTimeString()
-		}
-	})
-
-	document.querySelectorAll('fieldset.collapsed legend a').forEach(function(a) {
-		a.onclick = function() {
-			var parent = null
-			for (parent = this.parentNode; parent != null; parent = parent.parentNode) {
-				if (parent.nodeName === 'FIELDSET') {
-					parent.querySelector('div.fields').style.display = 'block'
-				}
-			}
-
-			this.remove()
-			return false
-		}
-	})
+	var buffer = 1 // spacing for error message
 
 	document.querySelectorAll('input[type="password"]').forEach(function(input) {
-		var name = input.getAttribute("id")
-		var passwordPrimary
-		var passwordConfirm
-		var shouldCheckPwned = ( input.getAttribute("data-sqmgr") === "no-pwned" )
-		var pwnedReq
+		var id = input.getAttribute('id')
+		var confirmInput, checkPasswordFn, noMatchElem
 
-		if (name.indexOf("confirm-") === 0) {
-			passwordPrimary = document.getElementById(name.substr(8))
-			passwordConfirm = input
-		} else {
-			passwordPrimary = input
-			passwordConfirm = document.getElementById('confirm-' + name)
-		}
-
-		if (!passwordPrimary || !passwordConfirm) {
+		if (id.indexOf('confirm-') === 0) {
 			return
 		}
 
-		var checkConfirmation = function() {
-			var pwnedCount = 0
-			if (passwordPrimary.value !== passwordConfirm.value) {
-				passwordConfirm.setCustomValidity("passwords do not match")
-				passwordConfirm.classList.add('passwords-no-match')
-			} else {
-				passwordPrimary.setCustomValidity("")
-				passwordPrimary.classList.remove('password-pwned')
-				passwordConfirm.setCustomValidity("")
-				passwordConfirm.classList.remove('passwords-no-match')
-
-				if (pwnedCount = passwordPrimary.getAttribute('data-pwned')) {
-					passwordPrimary.setCustomValidity("password has been compromised " + pwnedCount + " time(s). Please choose another password")
-					passwordPrimary.classList.add('password-pwned')
-				}
-			}
+		confirmInput = document.getElementById('confirm-'+id)
+		if (!confirmInput) {
+			return
 		}
 
-		var checkPwned = function() {
-			input.removeAttribute("data-pwned")
+		checkPasswordFn = function() {
+			var clientRect
+			if (input.value === confirmInput.value) {
+				if (noMatchElem) {
+					noMatchElem.remove()
+					noMatchElem = null
+				}
 
-			if (pwnedReq) {
-				pwnedReq.abort()
-			}
-
-			password = input.value
-			if (password.length < 2) {
+				confirmInput.setCustomValidity("")
 				return
 			}
 
-			pwnedReq = new XMLHttpRequest()
-			pwnedReq.open('POST', '/pwned', true)
-			pwnedReq.onreadystatechange = function() {
-				var count
-				if (pwnedReq.readyState === 4) {
-					count = parseInt(pwnedReq.responseText, 10)
-					if (count > 0) {
-						checkConfirmation()
-						return
-					}
-				}
+			confirmInput.setCustomValidity("Passwords do not match")
+
+			if (noMatchElem) {
+				return
 			}
 
-			pwnedReq.send(password)
+			clientRect = confirmInput.getBoundingClientRect()
+
+			noMatchElem = document.createElement('div')
+			noMatchElem.textContent = 'The passwords do not match'
+			noMatchElem.style.left = clientRect.left+'px'
+			noMatchElem.style.top = clientRect.top+clientRect.height+buffer+'px'
+			noMatchElem.classList.add('input-error')
+			document.body.appendChild(noMatchElem)
 		}
 
-		input.addEventListener('blur', checkConfirmation)
-		input.addEventListener('keyup', checkConfirmation)
-
-		if (shouldCheckPwned) {
-			input.addEventListener('blur', checkPwned)
-		}
-	})
-
-	document.querySelectorAll('input[type="password"][data-sqmgr="no-pwned"]').forEach(function(input) {
-
-		input.onblur = function() {
-		}
+		input.addEventListener('keyup', checkPasswordFn)
+		confirmInput.addEventListener('keyup', checkPasswordFn)
 	})
 })
