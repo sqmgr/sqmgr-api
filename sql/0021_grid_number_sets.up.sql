b09de44c54ef10be60d24130e1bc3f38a8d314fb
Tom Peters <weters@me.com>
2026-01-24 14:22:17 -0500

    Do not change old migration

--- sql/0021_grid_number_sets.up.sql
+++ sql/0021_grid_number_sets.up.sql
@@ -17,8 +17,8 @@ BEGIN;
 -- Enum for number set configurations
 CREATE TYPE number_set_config AS ENUM (
     'standard', -- Standard (one set for all)
-    '1234',     -- 1st, 2nd, 3rd, 4th quarter
-    '123f',     -- 1st, 2nd, 3rd, Final
+    'q1234',     -- 1st, 2nd, 3rd, 4th quarter
+    'q123f',     -- 1st, 2nd, 3rd, Final
     'hf',       -- Half, Final
     'h4'        -- Half, 4th
 );
@@ -29,28 +29,30 @@ CREATE TYPE number_set_type AS ENUM (
 );
 
 -- Add configuration column to pools table (pool-level config)
-ALTER TABLE pools ADD COLUMN number_set_config number_set_config NOT NULL DEFAULT 'standard';
+ALTER TABLE pools
+    ADD COLUMN number_set_config number_set_config NOT NULL DEFAULT 'standard';
 
 -- New table for multiple number sets per grid
-CREATE TABLE grid_number_sets (
-    id              bigserial NOT NULL PRIMARY KEY,
-    grid_id         bigint NOT NULL REFERENCES grids(id) ON DELETE CASCADE,
-    set_type        number_set_type NOT NULL,
-    home_numbers    integer[],
-    away_numbers    integer[],
-    manual_draw     boolean NOT NULL DEFAULT false,
-    created         timestamp NOT NULL DEFAULT (now() at time zone 'utc'),
-    modified        timestamp NOT NULL DEFAULT (now() at time zone 'utc'),
-    UNIQUE(grid_id, set_type)
+CREATE TABLE grid_number_sets
+(
+    id           bigserial       NOT NULL PRIMARY KEY,
+    grid_id      bigint          NOT NULL REFERENCES grids (id) ON DELETE CASCADE,
+    set_type     number_set_type NOT NULL,
+    home_numbers integer[],
+    away_numbers integer[],
+    manual_draw  boolean         NOT NULL DEFAULT false,
+    created      timestamp       NOT NULL DEFAULT (now() at time zone 'utc'),
+    modified     timestamp       NOT NULL DEFAULT (now() at time zone 'utc'),
+    UNIQUE (grid_id, set_type)
 );
 
-CREATE INDEX grid_number_sets_grid_id_idx ON grid_number_sets(grid_id);
+CREATE INDEX grid_number_sets_grid_id_idx ON grid_number_sets (grid_id);
 
 -- Migrate existing grids with numbers to new table
 -- Cast text[] to integer[] since the original grids table stores numbers as text
 INSERT INTO grid_number_sets (grid_id, set_type, home_numbers, away_numbers, manual_draw)
-SELECT id, 'all'::number_set_type,
-       ARRAY(SELECT unnest(home_numbers)::integer),
+SELECT id,
+       'all'::number_set_type, ARRAY(SELECT unnest(home_numbers)::integer),
        ARRAY(SELECT unnest(away_numbers)::integer),
        manual_draw
 FROM grids
@@ -65,25 +67,29 @@ CREATE FUNCTION new_pool(_token text, _user_id bigint, _name text, _grid_type te
 AS
 $$
 DECLARE
-    _row     pools;
-    _counter integer := 0;
+_row     pools;
+    _counter
+integer := 0;
 BEGIN
-    INSERT INTO pools (token, user_id, name, grid_type, password_hash, number_set_config)
-    VALUES (_token, _user_id, _name, _grid_type, _password_hash, _number_set_config) RETURNING * INTO _row;
+INSERT INTO pools (token, user_id, name, grid_type, password_hash, number_set_config)
+VALUES (_token, _user_id, _name, _grid_type, _password_hash, _number_set_config) RETURNING *
+INTO _row;
 
-    LOOP
-        EXIT WHEN _counter = _squares;
+LOOP
+EXIT WHEN _counter = _squares;
 
         -- +1 because the square IDs are 1-based not 0-based
-        INSERT INTO pool_squares (pool_id, square_id)
-        VALUES (_row.id, _counter + 1);
+INSERT INTO pool_squares (pool_id, square_id)
+VALUES (_row.id, _counter + 1);
 
-        _counter := _counter + 1;
-    END LOOP;
+_counter
+:= _counter + 1;
+END LOOP;
 
-    PERFORM new_grid(_row.id, 999999);
+    PERFORM
+new_grid(_row.id, 999999);
 
-    RETURN _row;
+RETURN _row;
 END;
 $$;
 
